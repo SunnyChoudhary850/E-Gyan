a49feddcfd452921961b1c598cf4a587ef623cf6
SunnyChoudhary850 <royalsunny850@gmail.com>
2026-05-04 12:40:56 +0530

    Delete EnquiryRepository.java

--- EnquiryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.nouapp.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.project.nouapp.model.Enquiry;
-
-public interface EnquiryRepository extends JpaRepository<Enquiry,Integer>{
-
-}
