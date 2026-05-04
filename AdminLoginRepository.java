367e3bcdda16dd8459047b3a2e04ed436d0f3d85
SunnyChoudhary850 <royalsunny850@gmail.com>
2026-05-04 12:40:09 +0530

    Delete AdminLoginRepository.java

--- AdminLoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.nouapp.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.nouapp.model.AdminLogin;
-
-public interface AdminLoginRepository extends JpaRepository<AdminLogin, String> {
-
-}
