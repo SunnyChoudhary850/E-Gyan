65ab1c0dfe81e68cf20d7df5ccd435c788811456
SunnyChoudhary850 <royalsunny850@gmail.com>
2026-05-04 12:42:12 +0530

    Delete StudentInfoRepository.java

--- StudentInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.nouapp.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.project.nouapp.model.StudentInfo;
-
-public interface StudentInfoRepository  extends JpaRepository<StudentInfo,Long>{
-
-}
