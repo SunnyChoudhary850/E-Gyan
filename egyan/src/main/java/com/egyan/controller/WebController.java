5739a2dc5b2d6ef64da00da350e737ff792a72ce
Sunny Choudhary <royalsunny850@gmail.com>
2026-06-03 12:44:07 +0530

    Create WebController.java

---
+++ egyan/src/main/java/com/egyan/controller/WebController.java
@@ -0,0 +1,17 @@
+package com.egyan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+    @GetMapping("/")
+    public String home() {
+        return "forward:/index.html";
+    }
+
+    @GetMapping("/egyan")
+    public String platform() {
+        return "forward:/egyan.html";
+    }
+}
